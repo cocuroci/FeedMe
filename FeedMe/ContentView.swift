411601d491505e059bde6f2c3f240ec2c52847ae
Andre Cocuroci <andremartins@luizalabs.com>
2023-08-22 15:35:52 -0300

    First commit

---
+++ FeedMe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FeedMe
+//
+//  Created by Andre on 22/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FeedMe/FeedMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedMeApp.swift
+//  FeedMe
+//
+//  Created by Andre on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FeedMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
